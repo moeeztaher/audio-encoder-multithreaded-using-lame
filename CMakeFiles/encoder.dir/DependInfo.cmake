0b6dfd393b596aaba64426201c12a4fd89660c01
Moeez Tahir <moeztahir96@gmail.com>
2023-10-29 19:53:13 +0100

    multi test

---
+++ CMakeFiles/encoder.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yasir/Desktop/cinemo/src/encoder.cpp" "/home/yasir/Desktop/cinemo/CMakeFiles/encoder.dir/src/encoder.cpp.o"
+  "/home/yasir/Desktop/cinemo/src/fs.cpp" "/home/yasir/Desktop/cinemo/CMakeFiles/encoder.dir/src/fs.cpp.o"
+  "/home/yasir/Desktop/cinemo/src/lame_wrapper.cpp" "/home/yasir/Desktop/cinemo/CMakeFiles/encoder.dir/src/lame_wrapper.cpp.o"
+  "/home/yasir/Desktop/cinemo/src/log.cpp" "/home/yasir/Desktop/cinemo/CMakeFiles/encoder.dir/src/log.cpp.o"
+  "/home/yasir/Desktop/cinemo/src/main.cpp" "/home/yasir/Desktop/cinemo/CMakeFiles/encoder.dir/src/main.cpp.o"
+  "/home/yasir/Desktop/cinemo/src/wav.cpp" "/home/yasir/Desktop/cinemo/CMakeFiles/encoder.dir/src/wav.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/encoder.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encoder.dir/src/encoder.cpp.o"
+  "CMakeFiles/encoder.dir/src/fs.cpp.o"
+  "CMakeFiles/encoder.dir/src/lame_wrapper.cpp.o"
+  "CMakeFiles/encoder.dir/src/log.cpp.o"
+  "CMakeFiles/encoder.dir/src/main.cpp.o"
+  "CMakeFiles/encoder.dir/src/wav.cpp.o"
+  "encoder"
+  "encoder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
